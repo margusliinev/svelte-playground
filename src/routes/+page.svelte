f993339de22c15daab01cf76c7f690dd4aa8aaf3
Margus Liinev <margus.liinev@pipedrive.com>
2025-03-22 23:09:08 +0200

    add custom css variables for theming

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<main class="grid h-screen w-screen place-items-center bg-zinc-900 text-white">
+<main class="grid h-screen w-screen place-items-center">
     <h1 class="font-sans text-4xl">Welcome to SvelteKit</h1>
 </main>
