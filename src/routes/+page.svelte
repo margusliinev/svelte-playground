d7d01071738052e0d3381263185ff5d1cc018e29
Margus Liinev <margus.liinev@pipedrive.com>
2025-03-23 11:16:53 +0200

    remove double font setting and create basic counter for home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
+<script lang="ts">
+    let count = $state(0);
+
+    function increment() {
+        count += 1;
+    }
+</script>
+
 <main class="grid h-screen w-screen place-items-center">
-    <h1 class="font-sans text-4xl">Welcome to SvelteKit</h1>
+    <div class="flex flex-col items-center space-y-8">
+        <h1 class="text-4xl">Svelte Playground</h1>
+        <button onclick={increment} type="button" class="h-12 w-32 cursor-pointer rounded-lg bg-orange-600 text-lg text-zinc-50 transition-colors hover:bg-orange-700">Count is {count}</button>
+    </div>
 </main>
