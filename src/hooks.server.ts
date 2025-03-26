7dfd572e20728e9f4c4cf8f742804f0113cf6723
Margus Liinev <margus.liinev@pipedrive.com>
2025-03-26 22:40:37 +0200

    update packages, remove theme toggle, update static

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,9 @@
 import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
-export const themeHook = (async ({ event, resolve }) => {
-    let theme = event.cookies.get('theme');
-
-    if (theme !== 'dark' && theme !== 'light') {
-        theme = 'light';
-    }
-
-    return await resolve(event, {
-        transformPageChunk: ({ html }) => html.replace('data-theme=""', `data-theme="${theme}"`),
-    });
+export const RequestLogger = (async ({ event, resolve }) => {
+    console.log(`Method: ${event.request.method} Route: ${event.route.id}`);
+    return await resolve(event);
 }) satisfies Handle;
 
-export const handle = sequence(themeHook);
+export const handle = sequence(RequestLogger);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
 <main class="grid h-screen w-screen place-items-center">
     <div class="flex flex-col items-center space-y-8">
-        <h1 class="text-4xl">Svelte Playground</h1>
-        <button onclick={increment} type="button" class="h-12 w-32 cursor-pointer rounded-lg bg-orange-600 text-lg text-zinc-50 transition-colors hover:bg-orange-700">Count is {count}</button>
+        <h1 class="text-4xl font-semibold">Svelte Playground</h1>
+        <button type="button" onclick={increment} class="cursor-pointer rounded-lg bg-sky-600 px-4 py-2 text-lg transition-colors hover:bg-sky-500">Count is {count}</button>
     </div>
 </main>
